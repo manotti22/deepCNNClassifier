9ce47f6db0f6d63ead65b7ea07c13ebaba3a35a3
manotti22 <ottizoubairi@gmail.com>
2022-09-20 23:21:14 +0200

    stage 03  added

--- src/DeepClassifier/components/__init__.py
+++ src/DeepClassifier/components/__init__.py
@@ -1,2 +1,4 @@
 from DeepClassifier.components.data_ingestion import DataIngestion
-from DeepClassifier.components import PrepareBaseModel
+from DeepClassifier.components.prepare_base_model import PrepareBaseModel
+from DeepClassifier.components.prepare_callback import PrepareCallback
+from DeepClassifier.components.training import Training
--- src/DeepClassifier/entity/__init__.py
+++ src/DeepClassifier/entity/__init__.py
@@ -1,1 +1,5 @@
-from DeepClassifier.entity.config_entity import DataIngestionConfig, PrepareBaseModelConfig
+from DeepClassifier.entity.config_entity import (DataIngestionConfig, 
+PrepareBaseModelConfig,
+PrepareCallbacksConfig,
+TrainingConfig
+)
